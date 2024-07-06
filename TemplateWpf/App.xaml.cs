f62466974bcf4008f27188481e31695ca771c55d
gclove2000 <1123448674@qq.com>
2024-07-06 20:25:14 +0800

    添加全局异常捕捉

--- TemplateWpf/App.xaml.cs
+++ TemplateWpf/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Configuration;
 using System.Data;
+using System.Text;
 using System.Windows;
+using System.Windows.Threading;
+using TemplateWpf.Utils;
 
 namespace TemplateWpf
 {
@@ -12,12 +15,81 @@ namespace TemplateWpf
     public partial class App : Application
     {
         public readonly static ServiceProvider ServiceProvider;
+        public App()
+        {
 
+            this.Startup += new StartupEventHandler(App_Startup);
+            this.Exit += new ExitEventHandler(App_Exit);
+        }
         static App()
         {
             IServiceCollection services = new ServiceCollection();
             ServiceProvider = services.BuildServiceProvider();
+            //首先注册开始和退出事件
             //var res = ServiceProvider.GetService<MainWindowViewModel>();
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            //UI线程未捕获异常处理事件
+            this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+            //Task线程内未捕获异常处理事件
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            //非UI线程未捕获异常处理事件
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        void App_Exit(object sender, ExitEventArgs e)
+        {
+            //程序退出时需要处理的业务
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true; //把 Handled 属性设为true，表示此异常已处理，程序可以继续运行，不会强制退出      
+                MsgHelper.Error("UI线程异常:" + e.Exception.Message);
+                //MessageBox.Show("UI线程异常:" + e.Exception.Message);
+            }
+            catch (Exception ex)
+            {
+                //此时程序出现严重异常，将强制结束退出
+                //MessageBox.Show("UI线程发生致命错误！");
+                MsgHelper.FatalGlobal("UI线程发生致命错误！" + ex.ToString());
+
+            }
+
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            StringBuilder sbEx = new StringBuilder();
+            if (e.IsTerminating)
+            {
+                sbEx.Append("非UI线程发生致命错误");
+            }
+            sbEx.Append("非UI线程异常：");
+            if (e.ExceptionObject is Exception)
+            {
+                sbEx.Append(((Exception)e.ExceptionObject).Message);
+            }
+            else
+            {
+                sbEx.Append(e.ExceptionObject);
+            }
+            //MessageBox.Show(sbEx.ToString());
+            MsgHelper.Error(sbEx.ToString());
+
+        }
+
+        void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            //task线程内未处理捕获
+            MsgHelper.Error("Task线程异常：" + e.Exception.Message);
+
+            //MessageBox.Show("Task线程异常：" + e.Exception.Message);
+            e.SetObserved();//设置该异常已察觉（这样处理后就不会引起程序崩溃）
         }
     }
 
