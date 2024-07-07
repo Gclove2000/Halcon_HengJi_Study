b2d259c05ce97fb6cb5f1d812da30dfd338053be
gclove2000 <1123448674@qq.com>
2024-07-07 15:01:48 +0800

    初始化机器视觉复合项目

---
+++ MachineVision/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MachineVision
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return null;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry services)
+        {
+
+        }
+    }
+
+}
